b5d53fbfe5f4f04fd36e38d21b051cfb70a7d368
Patti Crosswait <patti.crosswait@gmail.com>
2020-08-20 14:09:43 -0500

    Add files via upload

---
+++ Customer.sql
@@ -0,0 +1,28 @@
+USE [AdventureWorks2019]
+GO
+
+/****** Object:  Table [dbo].[Customer]    Script Date: 8/20/2020 1:56:45 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Customer](
+	[CustomerId] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [varchar](50) NULL,
+	[LastName] [varchar](50) NULL,
+	[Address] [varchar](100) NULL,
+	[InsertedBy] [varchar](30) NULL,
+	[InsertDate] [date] NULL,
+	[UpdatedBy] [varchar](30) NULL,
+	[UpdateDate] [date] NULL,
+	[SSN] [varchar](11) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[CustomerId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
